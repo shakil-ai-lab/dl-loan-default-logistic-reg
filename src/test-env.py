daf9bc0defb56a197ce77d013c2aee1435c7540e
unknown <sanjaralap@gmail.com>
2025-07-08 22:29:36 -0700

    add some project structure and pkl files

--- src/test-env.py
+++
@@ -1,6 +0,0 @@
-# import sys
-# print("Python version:", sys.version)
-# print("Running in Conda env?:", "conda" in sys.version.lower())
-import numpy, pandas
-print("NumPy:", numpy.__version__)
-print("Pandas:", pandas.__version__)
